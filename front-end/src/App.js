36ac34d4ad49e0a42fa8f01c3e2046e6993c8626
tje0714 <tje0714@outlook.kr>
2025-12-05 11:20:22 +0900

    project start

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+// TODO: 필요한 컴포넌트들을 import 하세요
+
+function App() {
+    return (
+        <div>
+            {/* TODO: Router 설정을 완성하세요 */}
+        </div>
+    );
+}
